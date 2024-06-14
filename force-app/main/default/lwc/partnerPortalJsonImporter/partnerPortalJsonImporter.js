57dee3c6aaf027f1c460800ad2535a7bfb6da5e9
peterfriberg-metaforce <peter.friberg@metaforce.se>
2024-06-14 18:15:02 +0200

    Add and update custom fields, objects, and classes
    Two custom members 'Opportunity' and 'OpportunityLineItem' have been added. New file modes have been created for 'Opportunity' and 'OpportunityLineItem' object metadata. Relevant fields have been updated in the 'OpportunityProductMapper' and 'PortalJsonParser' classes. Additional changes include the update in JSON file handling in the 'partnerPortalJsonImporter' Javascript file.

--- force-app/main/default/lwc/partnerPortalJsonImporter/partnerPortalJsonImporter.js
+++ force-app/main/default/lwc/partnerPortalJsonImporter/partnerPortalJsonImporter.js
@@ -5,24 +5,65 @@
 import { api, track, LightningElement } from 'lwc';
 import apexParseJson from '@salesforce/apex/PortalJsonController.parseJson';
 import apexParseJsonAndMapProduct from '@salesforce/apex/PortalJsonController.parseJsonAndMapProduct';
+import apexConvertQuote from '@salesforce/apex/PortalQuoteConverter.convertQuote';
+import apexGetJsonFile from '@salesforce/apex/PortalQuoteConverter.getJsonFile';
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
 export default class PartnerPortalJsonImporter extends LightningElement {
 
     @api recordId;
+
+    isLoading = true;
+    conversionFactor = 1;
 
     @track jsonText;
     @track data = {
         orderItems: []
     };
     @track mapping = [];
+    @track conversionResult = {
+        productLog: [],
+        pbeLog: []
+    };
 
     connectedCallback() {
         console.log('connectedCallback', this.recordId);
+        this.isLoading = false;
         debugger;
+    }
+
+    get acceptedFormats() {
+        return [ '.json' ];
+    }
+
+    async handleUploadFinished(event) {
+        // Get the list of uploaded files
+        const uploadedFiles = event.detail.files;
+        console.log('uploadedFiles', uploadedFiles);
+        // Show success message
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: uploadedFiles.length + ' Files uploaded successfully: ' + uploadedFiles[0].name,
+            variant: 'success',
+        });
+        this.dispatchEvent(toastEvent);
+        // Get the file
+        let result = await apexGetJsonFile({
+            contentBodyId: uploadedFiles[0].contentBodyId,
+            contentVersionId: uploadedFiles[0].contentVersionId,
+            documentId: uploadedFiles[0].documentId,
+            oppId: this.recordId
+        });
+        console.log('result', result);
+        this.jsonText = result;
     }
 
     updateJson(event) {
         this.jsonText = event.target.value;
+    }
+
+    updateConversionFactor(event) {
+        this.conversionFactor = event.target.value;
     }
 
     async handleImport() {
@@ -39,5 +80,59 @@ export default class PartnerPortalJsonImporter extends LightningElement {
         const result = await apexParseJsonAndMapProduct({jsonText: this.jsonText});
         console.log('result', result);
         this.mapping = result;
+    }
+
+    async handleProducts() {
+        this.isLoading = true;
+        console.log('handleProducts');
+        // Call apex method to parse JSON
+        try {
+            const result = await apexConvertQuote({
+                jsonText: this.jsonText,
+                oppId: this.recordId,
+                createProducts: false,
+                conversionFactor: this.conversionFactor
+            });
+            console.log('result', result);
+            this.conversionResult = result;
+            this.isLoading = false;
+        }
+        catch (error) {
+            console.error('error', error);
+            this.conversionResult = {
+                productLog: [
+                    JSON.stringify(error.body)
+                ],
+                pbeLog: []
+            };
+            this.isLoading = false;
+        }
+    }
+
+    async createProducts() {
+        this.isLoading = true;
+        console.log('createProducts');
+        // Call apex method to parse JSON
+        try {
+            const result = await apexConvertQuote({
+                jsonText: this.jsonText,
+                oppId: this.recordId,
+                createProducts: true,
+                conversionFactor: this.conversionFactor
+            });
+            console.log('result', result);
+            this.conversionResult = result;
+            this.isLoading = false;
+        }
+        catch (error) {
+            console.error('error', error);
+            this.conversionResult = {
+                productLog: [
+                    JSON.stringify(error.body)
+                ],
+                pbeLog: []
+            };
+            this.isLoading = false;
+        }
     }
 }
