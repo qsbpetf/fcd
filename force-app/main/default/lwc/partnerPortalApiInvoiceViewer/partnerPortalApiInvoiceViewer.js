650d400731ace5edb7e29f0500eca8653c9c556d
peterfriberg-metaforce <peter.friberg@metaforce.se>
2024-08-15 09:24:09 +0200

    Add invoice fetching and display for partner portal
    Implemented a new method in `PortalCommerceApiController` to fetch invoices based on account ID. Introduced the `partnerPortalApiInvoiceViewer` component to display fetched invoices in the partner portal, including necessary HTML, CSS, and JavaScript files for functionality and styling.

---
+++ force-app/main/default/lwc/partnerPortalApiInvoiceViewer/partnerPortalApiInvoiceViewer.js
@@ -0,0 +1,56 @@
+/**
+ * Created by peterfriberg on 2024-08-15.
+ */
+
+import { api, LightningElement, track } from 'lwc';
+import apexConvertInvoice from '@salesforce/apex/PortalCommerceApiController.getInvoices';
+
+export default class PartnerPortalApiInvoiceViewer extends LightningElement {
+
+    @api recordId;
+    @api errorMessage;
+
+    isLoading = true;
+
+    @track invoiceResults = {
+        data: [],
+        nextId: null
+    };
+
+    connectedCallback() {
+        console.log('connectedCallback', this.recordId);
+        debugger;
+        this.getInvoices();
+    }
+
+    getInvoices() {
+        console.log('getInvoices', this.recordId);
+        this.isLoading = true;
+        apexConvertInvoice({ accountId: this.recordId })
+            .then(result => {
+                console.log('result', result);
+                this.invoiceResults = this.calculate(result);
+                this.isLoading = false;
+            })
+            .catch(error => {
+                console.error('error', error);
+                this.isLoading = false;
+                this.invoiceResults = {
+                    error: JSON.stringify(error),
+                    data: [],
+                    nextId: null,
+                    missingAccountId: true
+                };
+            });
+    }
+
+    calculate(result) {
+        result.data.forEach(invoice => {
+            invoice.length = invoice.items.length;
+        });
+        if (result.missingAccountId && (result.error === undefined || result.error === null || result.error === '')) {
+            result.error = this.errorMessage;
+        }
+        return result;
+    }
+}
