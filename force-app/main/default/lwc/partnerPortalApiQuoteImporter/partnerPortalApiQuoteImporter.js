0ede85740e9da9b1649481cebe6ac8ca7e78795c
peterfriberg-metaforce <peter.friberg@metaforce.se>
2024-08-16 16:08:59 +0200

    Add tree grid to display quotes with enhanced columns
    Added a `lightning-tree-grid` to display quote results, along with new columns for created and expire dates. Updated styling for numerical and currency fields and included row action handling to support quote import functionality. Major changes include updating the API version in the metadata file and introducing new CSS classes for better alignment.

--- force-app/main/default/lwc/partnerPortalApiQuoteImporter/partnerPortalApiQuoteImporter.js
+++ force-app/main/default/lwc/partnerPortalApiQuoteImporter/partnerPortalApiQuoteImporter.js
@@ -10,17 +10,74 @@ export default class PartnerPortalApiQuoteImporter extends LightningElement {
     @api recordId;
     @api errorMessage;
 
-    isLoading = true;
-
     @track quoteResults = {
         data: [],
         nextId: null
     };
 
+    @track selectedItem = { id: '' }; // Used to store the clicked row data handleRowClick(event) {
+    @track content;
+
+    isLoading = true;
+    columns = [
+        { label: 'Quote Number', fieldName: 'quoteNumber', type: 'text' },
+        { label: '# Items', fieldName: 'length', type: 'number' },
+        { label: 'Status', fieldName: 'status', type: 'text',
+            cellAttributes: {
+                class: { fieldName: 'statusClass' }
+            }
+        },
+        { label: 'Created Date', fieldName: 'createdDate', type: 'text' },
+        { label: 'Expire Date', fieldName: 'expireDate', type: 'text' },
+        { label: 'Description', fieldName: 'description', type: 'text' },
+        { label: 'Quantity', fieldName: 'quantity', type: 'number' },
+        {
+            label: 'Subtotal',
+            fieldName: 'subTotal',
+            type: 'currency',
+            typeAttributes: {
+                currencyCode: { fieldName: 'isoCurrency' }
+            }
+        },
+        {
+            label: 'Total',
+            fieldName: 'total',
+            type: 'currency',
+            typeAttributes: {
+                currencyCode: { fieldName: 'isoCurrency' }
+            }
+        },
+        {
+            type: 'button',
+            label: 'Import',
+            typeAttributes: {
+                iconName: 'utility:download',
+                name: 'download',
+                label: { fieldName: 'action' },
+                title: { fieldName: 'action' },
+                variant: 'bare'
+            },
+            initialWidth: 120
+        }
+    ];
+
     connectedCallback() {
         console.log('connectedCallback', this.recordId);
         debugger;
         this.getQuotes();
+    }
+
+    // Method to handle the row action
+    handleRowAction(event) {
+        this.selectedItem = event.detail.row;
+        const action = event.detail.action;
+        console.log('Action: ', action.label);
+        console.log('Selected Row: ', this.selectedItem, JSON.stringify(this.selectedItem, null, 2));
+        this.content = JSON.stringify(this.selectedItem, null, 2);
+
+        if (this.selectedItem.level === 1) {
+            let choice = confirm('Are you sure you want to import this quote ' + this.selectedItem.quoteNumber + ' ?');
+        }
     }
 
     getQuotes() {
@@ -47,6 +104,23 @@ export default class PartnerPortalApiQuoteImporter extends LightningElement {
     calculate(result) {
         result.data.forEach(quote => {
             quote.length = quote.upcomingBills.lines.length;
+            // convert unix time milli seconds since epoch to Date object
+            quote.createdDate = new Date(quote.createdAt).toLocaleDateString(
+                'sv-SV', { year: 'numeric', month: 'numeric', day: 'numeric' });
+            quote.expireDate = new Date(quote.expiresAt).toLocaleDateString(
+                'sv-SV', { year: 'numeric', month: 'numeric', day: 'numeric' });
+            quote.statusClass = quote.status.includes('ACCEPTED') ? 'slds-text-color_success' : quote.status.includes('CANCELLED') ? 'slds-text-color_error' : '';
+            quote._children = [];
+            quote.upcomingBills.lines.forEach(item => {
+                quote._children.push({
+                    id: item.id,
+                    description: item.description,
+                    quantity: item.quantity,
+                    subTotal: item.subTotal,
+                    total: item.total,
+                    isoCurrency: item.isoCurrency
+                });
+            });
         });
         if (result.missingAccountId && (result.error === undefined || result.error === null || result.error === '')) {
             result.error = this.errorMessage;
