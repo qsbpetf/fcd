64a56fd91f078cae73de6f40ea50d14da890d242
peterfriberg-metaforce <peter.friberg@metaforce.se>
2024-12-03 12:54:06 +0100

    Reformat code for consistent indentation
    Unified the indentation across multiple files for better readability and maintainability. Changes include converting two-space indentation to four-space in CSS, HTML, and JavaScript files, and updating string delimiters from double to single quotes where applicable. No functional code changes were made.

--- force-app/main/default/lwc/opportunityLineItemsDeleteModal/opportunityLineItemsDeleteModal.js
+++ force-app/main/default/lwc/opportunityLineItemsDeleteModal/opportunityLineItemsDeleteModal.js
@@ -1,89 +1,88 @@
-import { api } from "lwc";
-import LightningModal from "lightning/modal";
-import { ShowToastEvent } from "lightning/platformShowToastEvent";
-import { notifyRecordUpdateAvailable } from "lightning/uiRecordApi";
+import { api } from 'lwc';
+import LightningModal from 'lightning/modal';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
 
-import { formatOpportunityLineItems } from "c/formatUtils";
+import { formatOpportunityLineItems } from 'c/formatUtils';
 
-import deleteOpportunityLineItems from "@salesforce/apex/OpportunityLineItemController.deleteOpportunityLineItems";
-import getOpportunityLineItemsByIds from "@salesforce/apex/OpportunityLineItemController.getOpportunityLineItemsByIds";
+import deleteOpportunityLineItems from '@salesforce/apex/OpportunityLineItemController.deleteOpportunityLineItems';
+import getOpportunityLineItemsByIds from '@salesforce/apex/OpportunityLineItemController.getOpportunityLineItemsByIds';
 
 export default class OpportunityLineItemsDeleteModal extends LightningModal {
-  @api opportunityId;
-  @api opportunityLineItemIds;
+    @api opportunityId;
+    @api opportunityLineItemIds;
 
-  opportunityLineItems = [];
-  opportunityLineItemErrors = {};
+    opportunityLineItems = [];
+    opportunityLineItemErrors = {};
 
-  isSubmitting = false;
-  isFetching = false;
+    isSubmitting = false;
+    isFetching = false;
 
-  connectedCallback() {
-    this.handleOpportunityLineItemsGet();
-  }
-
-  async handleOpportunityLineItemsGet() {
-    this.isFetching = true;
-    const opportunityLineItems = await getOpportunityLineItemsByIds({
-      oliIds: this.opportunityLineItemIds
-    });
-    this.opportunityLineItems =
-      formatOpportunityLineItems(opportunityLineItems);
-    this.isFetching = false;
-  }
-
-  async handleSubmitClick() {
-    this.isSubmitting = true;
-    try {
-      const { ids, errors } = await deleteOpportunityLineItems({
-        oliIds: this.opportunityLineItemIds
-      });
-
-      await notifyRecordUpdateAvailable(ids.map((recordId) => ({ recordId })));
-      await notifyRecordUpdateAvailable([{ recordId: this.opportunityId }]);
-
-      this.opportunityLineItemErrors = errors;
-      if (!Object.keys(errors).length) {
-        this.close("submit");
-        this.handleSuccessToastShow();
-      }
-    } catch (error) {
-      console.error(error);
-      const message = error?.body?.message ?? "An unknown error occurred";
-      this.handleErrorToastShow(message);
+    connectedCallback() {
+        this.handleOpportunityLineItemsGet();
     }
-    this.isSubmitting = false;
-  }
 
-  handleErrorToastShow(message) {
-    const event = new ShowToastEvent({
-      title: "An error occurred",
-      message,
-      variant: "error"
-    });
-    this.dispatchEvent(event);
-  }
+    async handleOpportunityLineItemsGet() {
+        this.isFetching = true;
+        const opportunityLineItems = await getOpportunityLineItemsByIds({
+            oliIds: this.opportunityLineItemIds,
+        });
+        this.opportunityLineItems = formatOpportunityLineItems(opportunityLineItems);
+        this.isFetching = false;
+    }
 
-  handleSuccessToastShow() {
-    const event = new ShowToastEvent({
-      title: "Success",
-      message: "Opportunity Products deleted",
-      variant: "success"
-    });
-    this.dispatchEvent(event);
-  }
+    async handleSubmitClick() {
+        this.isSubmitting = true;
+        try {
+            const { ids, errors } = await deleteOpportunityLineItems({
+                oliIds: this.opportunityLineItemIds,
+            });
 
-  handleCancelClick() {
-    this.close("cancel");
-  }
+            await notifyRecordUpdateAvailable(ids.map((recordId) => ({ recordId })));
+            await notifyRecordUpdateAvailable([{ recordId: this.opportunityId }]);
 
-  get computedOpportunityLineItems() {
-    return this.opportunityLineItems.map((opportunityLineItem) => {
-      const error = this.opportunityLineItemErrors?.[opportunityLineItem.Id];
-      return {
-        ...opportunityLineItem,
-        error
-      };
-    });
-  }
+            this.opportunityLineItemErrors = errors;
+            if (!Object.keys(errors).length) {
+                this.close('submit');
+                this.handleSuccessToastShow();
+            }
+        } catch (error) {
+            console.error(error);
+            const message = error?.body?.message ?? 'An unknown error occurred';
+            this.handleErrorToastShow(message);
+        }
+        this.isSubmitting = false;
+    }
+
+    handleErrorToastShow(message) {
+        const event = new ShowToastEvent({
+            title: 'An error occurred',
+            message,
+            variant: 'error',
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleSuccessToastShow() {
+        const event = new ShowToastEvent({
+            title: 'Success',
+            message: 'Opportunity Products deleted',
+            variant: 'success',
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleCancelClick() {
+        this.close('cancel');
+    }
+
+    get computedOpportunityLineItems() {
+        return this.opportunityLineItems.map((opportunityLineItem) => {
+            const error = this.opportunityLineItemErrors?.[opportunityLineItem.Id];
+            return {
+                ...opportunityLineItem,
+                error,
+            };
+        });
+    }
 }
--- force-app/main/default/lwc/opportunityLineItemsEditDatatable/opportunityLineItemsEditDatatable.js
+++ force-app/main/default/lwc/opportunityLineItemsEditDatatable/opportunityLineItemsEditDatatable.js
@@ -1,255 +1,262 @@
-import { LightningElement, api, wire, track } from "lwc";
-import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 
-import CurrencyIsoCode from "@salesforce/schema/Opportunity.CurrencyIsoCode";
-import getTypePicklistValues from "@salesforce/apex/OpportunityLineItemController.getTypePicklistValues";
+import CurrencyIsoCode from '@salesforce/schema/Opportunity.CurrencyIsoCode';
+import getTypePicklistValues from '@salesforce/apex/OpportunityLineItemController.getTypePicklistValues';
 
 export default class OpportunityEditLineItemsDatatable extends LightningElement {
-  @api opportunityId;
-  @api opportunityLineItems = [];
-  @api opportunityLineItemErrors = {};
-  @api isFetching = false;
-  @api isSubmitting = false;
-  @api isDeletable = false;
+    @api opportunityId;
+    @api opportunityLineItems = [];
+    @api opportunityLineItemErrors = {};
+    @api isFetching = false;
+    @api isSubmitting = false;
+    @api isDeletable = false;
 
-  @track opportunityLineItemColumns = [];
-  @track types = [];
-  @track opportunity = {};
+    @track opportunityLineItemColumns = [];
+    @track types = [];
+    @track opportunity = {};
 
-  @wire(getRecord, {
-    recordId: "$opportunityId",
-    fields: [CurrencyIsoCode]
-  })
-  getOpportunityRecord({ data, error }) {
-    if (data) {
-      this.opportunity = {
-        CurrencyIsoCode: getFieldValue(data, CurrencyIsoCode)
-      };
-      this.handleOpportunityLineItemColumnsSet();
-    } else if (error) {
-      console.error(error);
-    }
-  }
-
-  @wire(getTypePicklistValues) getTypePicklistValuesWire({ data, error }) {
-    if (data) {
-      this.types = data;
-      this.handleOpportunityLineItemColumnsSet();
-    } else if (error) {
-      console.error(error);
-    }
-  }
-
-  connectedCallback() {
-    this.handleOpportunityLineItemColumnsSet();
-  }
-
-  handleOpportunityLineItemsChange(opportunityLineItems) {
-    const event = new CustomEvent("opportunitylineitemschange", {
-      detail: {
-        value: opportunityLineItems
-      }
-    });
-    this.dispatchEvent(event);
-  }
-
-  handlePhaseChange(phase) {
-    const event = new CustomEvent("phasechange", {
-      detail: {
-        value: phase
-      }
-    });
-    this.dispatchEvent(event);
-  }
-
-  handleBackClick() {
-    this.handlePhaseChange(1);
-  }
-
-  handleOpportunityLineItemsRowAction(event) {
-    const opportunityLineItemId = event?.detail?.row?.Id;
-    const opportunityLineItems = [...this.opportunityLineItems]?.filter(
-      (opportunityLineItem) => opportunityLineItem.Id !== opportunityLineItemId
-    );
-    this.handleOpportunityLineItemsChange(opportunityLineItems);
-  }
-
-  handleOpportunityLineItemsCellChange(event) {
-    const nextOpportunityLineItem = this.handleDraftValueFormat(
-      event?.detail?.draftValues?.[0]
-    );
-    const opportunityLineItems = [...this.opportunityLineItems]?.map(
-      (opportunityLineItem) => {
-        if (opportunityLineItem?.Id === nextOpportunityLineItem?.Id) {
-          return { ...opportunityLineItem, ...nextOpportunityLineItem };
+    @wire(getRecord, {
+        recordId: '$opportunityId',
+        fields: [CurrencyIsoCode],
+    })
+    getOpportunityRecord({ data, error }) {
+        if (data) {
+            this.opportunity = {
+                CurrencyIsoCode: getFieldValue(data, CurrencyIsoCode),
+            };
+            this.handleOpportunityLineItemColumnsSet();
+        } else if (error) {
+            console.error(error);
         }
-        return opportunityLineItem;
-      }
-    );
-    let areAllTypeFieldsPopulated = true;
-    opportunityLineItems.forEach((opportunityLineItem) => {
-      if (!opportunityLineItem.Renewal__c) {
-        areAllTypeFieldsPopulated = false;
-      }
-    });
-    this.opportunityLineItemColumns = [...this.opportunityLineItemColumns]?.map((c) => {
-      if (c.fieldName === "Renewal__c") {
-          return { ...c, label: areAllTypeFieldsPopulated ? "Type" : "Type *" };
-      }
-      return c;
-  });
-  
-    this.handleOpportunityLineItemsChange(opportunityLineItems);
-  }
-
-  handleDraftValueFormat(draftValue) {
-    const contact = draftValue.SEN_Technical_Contact__c;
-    if (contact?.includes("|")) {
-      const [contactId, contactName] = contact.split("|");
-      return {
-        ...draftValue,
-        SEN_Technical_Contact__c: contactId,
-        SEN_Technical_Contact__r: {
-          Name: contactName
-        }
-      };
     }
-    return draftValue;
-  }
 
-  handleOpportunityLineItemColumnsSet() {
-    this.opportunityLineItemColumns = [
-      {
-        label: "Product",
-        fieldName: "PricebookEntry.Product2.Name",
-        initialWidth: 300,
-        type: "text"
-      },
-      {
-        label: "Quantity",
-        fieldName: "Quantity",
-        type: "number",
-        initialWidth: 100,
-        editable: true
-      },
-      {
-        label: "Unit List Price",
-        fieldName: "UnitPrice",
-        type: "currency",
-        typeAttributes: {
-          currencyCode: this.opportunity?.CurrencyIsoCode
-        },
-        initialWidth: 150,
-        editable: true
-      },
-      {
-        label: "Discount",
-        fieldName: "Discount__c",
-        type: "discount",
-        typeAttributes: {
-          name: "Discount__c"
-        },
-        cellAttributes: {
-          alignment: "right"
-        },
-        initialWidth: 100,
-        editable: true
-      },
-      {
-        label: "Type *",
-        fieldName: "Renewal__c",
-        type: "picklist",
-        typeAttributes: {
-          name: "Renewal__c",
-          placeholder: "Select Type...",
-          options: this.computedTypes
-        },
-        initialWidth: 160,
-        editable: true
-      },
-      {
-        label: "Start Date",
-        fieldName: "License_Start_date__c",
-        type: "date-local",
-        initialWidth: 155,
-        editable: true
-      },
-      {
-        label: "End Date",
-        fieldName: "License_end_date__c",
-        type: "date-local",
-        initialWidth: 155,
-        editable: true
-      },
-      {
-        label: "Additional Info",
-        fieldName: "Additional_Info__c",
-        type: "text",
-        initialWidth: 160,
-        editable: true
-      },
-      {
-        label: "LIC: Editable Unit List Price in USD",
-        fieldName: "Editable_Unit_List_Price_in_USD__c",
-        type: "currency",
-        typeAttributes: {
-          currencyCode: "USD"
-        },
-        initialWidth: 150,
-        editable: true
-      },
-      {
-        label: "LIC: Editable Cost Price in USD",
-        fieldName: "Editable_Cost_Price_in_Dollars__c",
-        type: "currency",
-        typeAttributes: {
-          currencyCode: "USD"
-        },
-        initialWidth: 150,
-        editable: true
-      },
-      {
-        label: "LIC: SEN",
-        fieldName: "SEN__c",
-        type: "text",
-        initialWidth: 160,
-        editable: true
-      },
-      {
-        label: "LIC: Unusual Discount Reviewed",
-        fieldName: "Unusual_discount_reviewed__c",
-        type: "boolean",
-        initialWidth: 140,
-        editable: true
-      },
-      {
-        label: "LIC: Loss Reviewed",
-        fieldName: "Loss_Reviewed__c",
-        type: "boolean",
-        initialWidth: 140,
-        editable: true
-      }
-    ];
-    if (this.isDeletable) {
-      this.opportunityLineItemColumns.unshift({
-        label: "",
-        type: "button-icon",
-        fixedWidth: 70,
-        typeAttributes: {
-          iconName: "utility:delete",
-          alternativeText: "Delete",
-          name: "delete",
-          title: "Delete"
+    @wire(getTypePicklistValues) getTypePicklistValuesWire({ data, error }) {
+        if (data) {
+            this.types = data;
+            this.handleOpportunityLineItemColumnsSet();
+        } else if (error) {
+            console.error(error);
         }
-      });
     }
-  }
 
-  get isOpportunityLineItemsEmpty() {
-    return !this.opportunityLineItems?.length;
-  }
+    connectedCallback() {
+        this.handleOpportunityLineItemColumnsSet();
+    }
 
-  get computedTypes() {
-    return [{ label: "--None--", value: "" }, ...this.types];
-  }
+    handleOpportunityLineItemsChange(opportunityLineItems) {
+        const event = new CustomEvent('opportunitylineitemschange', {
+            detail: {
+                value: opportunityLineItems,
+            },
+        });
+        this.dispatchEvent(event);
+    }
+
+    handlePhaseChange(phase) {
+        const event = new CustomEvent('phasechange', {
+            detail: {
+                value: phase,
+            },
+        });
+        this.dispatchEvent(event);
+    }
+
+    handleBackClick() {
+        this.handlePhaseChange(1);
+    }
+
+    handleOpportunityLineItemsRowAction(event) {
+        const opportunityLineItemId = event?.detail?.row?.Id;
+        const opportunityLineItems = [...this.opportunityLineItems]?.filter(
+            (opportunityLineItem) => opportunityLineItem.Id !== opportunityLineItemId,
+        );
+        this.handleOpportunityLineItemsChange(opportunityLineItems);
+    }
+
+    handleOpportunityLineItemsCellChange(event) {
+        const nextOpportunityLineItem = this.handleDraftValueFormat(event?.detail?.draftValues?.[0]);
+        const opportunityLineItems = [...this.opportunityLineItems]?.map((opportunityLineItem) => {
+            if (opportunityLineItem?.Id === nextOpportunityLineItem?.Id) {
+                return { ...opportunityLineItem, ...nextOpportunityLineItem };
+            }
+            return opportunityLineItem;
+        });
+        let areAllTypeFieldsPopulated = true;
+        opportunityLineItems.forEach((opportunityLineItem) => {
+            if (!opportunityLineItem.Renewal__c) {
+                areAllTypeFieldsPopulated = false;
+            }
+        });
+        this.opportunityLineItemColumns = [...this.opportunityLineItemColumns]?.map((c) => {
+            if (c.fieldName === 'Renewal__c') {
+                return { ...c, label: areAllTypeFieldsPopulated ? 'Type' : 'Type *' };
+            }
+            return c;
+        });
+
+        this.handleOpportunityLineItemsChange(opportunityLineItems);
+    }
+
+    handleDraftValueFormat(draftValue) {
+        const contact = draftValue.SEN_Technical_Contact__c;
+        if (contact?.includes('|')) {
+            const [contactId, contactName] = contact.split('|');
+            return {
+                ...draftValue,
+                SEN_Technical_Contact__c: contactId,
+                SEN_Technical_Contact__r: {
+                    Name: contactName,
+                },
+            };
+        }
+        return draftValue;
+    }
+
+    handleOpportunityLineItemColumnsSet() {
+        this.opportunityLineItemColumns = [
+            {
+                label: 'Product',
+                fieldName: 'PricebookEntry.Product2.Name',
+                initialWidth: 300,
+                type: 'text',
+            },
+            {
+                label: 'Quantity',
+                fieldName: 'Quantity',
+                type: 'number',
+                initialWidth: 100,
+                editable: true,
+            },
+            {
+                label: 'Unit List Price',
+                fieldName: 'UnitPrice',
+                type: 'currency',
+                typeAttributes: {
+                    currencyCode: this.opportunity?.CurrencyIsoCode,
+                },
+                initialWidth: 150,
+                editable: true,
+            },
+            {
+                label: 'Discount',
+                fieldName: 'Discount__c',
+                type: 'discount',
+                typeAttributes: {
+                    name: 'Discount__c',
+                },
+                cellAttributes: {
+                    alignment: 'right',
+                },
+                initialWidth: 100,
+                editable: true,
+            },
+            {
+                label: 'Type *',
+                fieldName: 'Renewal__c',
+                type: 'picklist',
+                typeAttributes: {
+                    name: 'Renewal__c',
+                    placeholder: 'Select Type...',
+                    options: this.computedTypes,
+                },
+                initialWidth: 160,
+                editable: true,
+            },
+            {
+                label: 'Start Date',
+                fieldName: 'License_Start_date__c',
+                type: 'date-local',
+                initialWidth: 155,
+                editable: true,
+            },
+            {
+                label: 'End Date',
+                fieldName: 'License_end_date__c',
+                type: 'date-local',
+                initialWidth: 155,
+                editable: true,
+            },
+            {
+                label: 'Additional Info',
+                fieldName: 'Additional_Info__c',
+                type: 'text',
+                initialWidth: 160,
+                editable: true,
+            },
+            {
+                label: 'LIC: Editable Unit List Price in USD',
+                fieldName: 'Editable_Unit_List_Price_in_USD__c',
+                type: 'currency',
+                typeAttributes: {
+                    currencyCode: 'USD',
+                },
+                initialWidth: 150,
+                editable: true,
+            },
+            {
+                label: 'LIC: Editable Cost Price in USD',
+                fieldName: 'Editable_Cost_Price_in_Dollars__c',
+                type: 'currency',
+                typeAttributes: {
+                    currencyCode: 'USD',
+                },
+                initialWidth: 150,
+                editable: true,
+            },
+            {
+                label: 'LIC: SEN',
+                fieldName: 'SEN__c',
+                type: 'text',
+                initialWidth: 160,
+                editable: true,
+            },
+            {
+                label: "User Count",
+                fieldName: "User_Count__c",
+                type: "integer",
+                initialWidth: 120,
+                sortable: true,
+                cellAttributes: {
+                    alignment: "right"
+                },
+                editable: true,
+            },
+            {
+                label: 'LIC: Unusual Discount Reviewed',
+                fieldName: 'Unusual_discount_reviewed__c',
+                type: 'boolean',
+                initialWidth: 140,
+                editable: true,
+            },
+            {
+                label: 'LIC: Loss Reviewed',
+                fieldName: 'Loss_Reviewed__c',
+                type: 'boolean',
+                initialWidth: 140,
+                editable: true,
+            },
+        ];
+        if (this.isDeletable) {
+            this.opportunityLineItemColumns.unshift({
+                label: '',
+                type: 'button-icon',
+                fixedWidth: 70,
+                typeAttributes: {
+                    iconName: 'utility:delete',
+                    alternativeText: 'Delete',
+                    name: 'delete',
+                    title: 'Delete',
+                },
+            });
+        }
+    }
+
+    get isOpportunityLineItemsEmpty() {
+        return !this.opportunityLineItems?.length;
+    }
+
+    get computedTypes() {
+        return [{ label: '--None--', value: '' }, ...this.types];
+    }
 }
